97780cf83d99969e88149ebd4bc0b82b7d786ea9
flc-eclipse-cippool <fanli@cip.ifi.lmu.de>
2013-08-12 21:17:15 +0200

    first commit

---
+++ SpeechRecorderAndroid/src/com/srandroid/ActivityMain.java
@@ -0,0 +1,22 @@
+package com.srandroid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ActivityMain extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
