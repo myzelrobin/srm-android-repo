e4c35bb11483dd7619579289e6c015c9c6fb6404
flc <flc@flc-asus>
2013-08-23 08:55:32 +0200

    test

--- SpeechRecorderAndroid/src/com/srandroid/database/DBAccessor.java
+++ SpeechRecorderAndroid/src/com/srandroid/database/DBAccessor.java
@@ -15,7 +15,7 @@ import android.util.Log;
 public class DBAccessor extends SQLiteOpenHelper
 {
 	private static final String DATABASE_NAME = "srandroid_database.db";
-	private static final int DATABASE_VERSION = 5;
+	private static final int DATABASE_VERSION = 6;
 	
 
 	public DBAccessor(Context context) 
